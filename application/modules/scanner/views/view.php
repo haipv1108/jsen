e18d79f4a185961a3a11ed54dcfd70b5cb137142
nguyenngocha <dodaihoc.abvk@gmail.com>
2015-08-11 16:44:05 +0700

    delete

--- application/modules/scanner/views/view.php
+++
@@ -1,3 +0,0 @@
-﻿		<?php foreach($result as $key=>$val){?>
-				INSERT INTO `jsen`.`station` (`station_id`, `station_name`, `line_id`,`city_id`) VALUES(<?php echo $val['station_id'];?>,<?php echo $val['line_id'];?>,'<?php echo $val['station_name'];?>',<?php echo $val['city_id'];?>);<br/>
-		<?php } ?>
