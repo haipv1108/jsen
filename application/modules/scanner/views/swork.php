2feb65d5d1650338768e18fc7062f7ef81311f10
HaiLeader <pham.haisv@gmail.com>
2015-08-14 17:14:58 +0900

    Xoa scanner

--- application/modules/scanner/views/swork.php
+++
@@ -1,3 +0,0 @@
-		<?php foreach($result as $key=>$val){?>
-				INSERT INTO `jsen`.`line` (`line_id`, `line_name`, `area_line_id`) VALUES(<?php echo $val['line_id'];?>,'<?php echo $val['line_name'];?>',<?php echo $val['area_line_id'];?>);<br/>
-		<?php } ?>
